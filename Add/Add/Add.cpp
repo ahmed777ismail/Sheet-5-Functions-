ac8846f8fae37cb448f1bb4a8192221d30027d7d
ahmed ismail <87954455+ahmed777ismail@users.noreply.github.com>
2024-05-17 17:38:28 +0300

    Add files via upload
    Add

---
+++ Add/Add/Add.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int Add() {
+	int num1, num2;
+	cin >> num1 >> num2;
+	int result = num1 + num2;
+
+	cout << result << endl;
+
+	return 0;
+}
+
+
+
+int main()
+{
+	Add();
+
+}
+
